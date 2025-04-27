ca55bd8a876f6fe041b6dbb80676bab595adc005
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-04-27 18:31:41 +0500

    API: Create `UserController` with basic GET endpoint

---
+++ TaskManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("UserController is working!");
+        }
+    }
+}
