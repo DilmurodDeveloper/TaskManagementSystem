dcf2ccc75f8c8035e1d16d7d7d51d117a1476d14
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-04-28 19:19:26 +0500

    FEAT: Implemented JWT Authentication and integrated Swagger Authorization

--- TaskManagementSystem/Program.cs
+++ TaskManagementSystem/Program.cs
@@ -1,16 +1,51 @@
 using TaskManagementSystem.Data;
-using Microsoft.EntityFrameworkCore;
-using TaskManagementSystem.Exceptions;       
+using Microsoft.EntityFrameworkCore;      
 using TaskManagementSystem.Middleware;       
 using TaskManagementSystem.Repositories;
 using TaskManagementSystem.Services;
 using TaskManagementSystem.Profiles;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1", 
+        new OpenApiInfo { 
+            Title = "Task Management API", 
+            Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Bearer {token}'",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -21,6 +56,27 @@ builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<ITaskService, TaskService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+builder.Services
+  .AddAuthentication(options =>
+  {
+      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  })
+  .AddJwtBearer(options =>
+  {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+          ValidateIssuer = true,
+          ValidateAudience = true,
+          ValidateLifetime = true,
+          ValidateIssuerSigningKey = true,
+          ValidIssuer = builder.Configuration["Jwt:Issuer"],
+          ValidAudience = builder.Configuration["Jwt:Audience"],
+          IssuerSigningKey = new SymmetricSecurityKey(
+                                     Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+      };
+  });
 
 var app = builder.Build();
 
@@ -33,6 +89,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
