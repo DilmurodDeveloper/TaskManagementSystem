d6c04763fa6cfe182718b3e2b44d3ba4b1e5c43e
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2025-04-28 13:50:43 +0500

    FEAT: Add RequiredIfAttribute

---
+++ TaskManagementSystem/Validation/Attributes/RequiredIfAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class RequiredIfAttribute : ValidationAttribute
+{
+    private readonly string _dependentProperty;
+    private readonly object _targetValue;
+
+    public RequiredIfAttribute(string dependentProperty, object targetValue)
+    {
+        _dependentProperty = dependentProperty;
+        _targetValue = targetValue;
+    }
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var dependentProperty = validationContext.ObjectType.GetProperty(_dependentProperty);
+        if (dependentProperty == null)
+        {
+            return new ValidationResult($"Property {_dependentProperty} not found.");
+        }
+
+        var dependentValue = dependentProperty.GetValue(validationContext.ObjectInstance, null);
+        if (dependentValue != null && dependentValue.Equals(_targetValue) && value == null)
+        {
+            return new ValidationResult($"The {validationContext.DisplayName} field is required.");
+        }
+
+        return ValidationResult.Success;
+    }
+}
